b1146a64e3b1cee5e120ed226d9d72d462edc53c
Niraj Wadile <65112923+elementrix05@users.noreply.github.com>
2023-02-24 00:05:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://sharelinkproject.herokuapp.com";
+//const baseURL = "https://sharelinkproject.herokuapp.com";
+const baseURL = "https://wild-rose-barnacle-cap.cyclic.app/";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
